b195edc9f3d35daa184d516f5659ef89ac135c6d
Salisa17za <spoysalizz17@gmail.com>
2025-08-22 11:19:49 +0700

    คำอupdatever1

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-     <h2>Salisa Boonpa 67702879</h2>
+     <h2>Salisa Boonpa ID 67702879</h2>
+      <h2>Information Technology</h2>
   </div>
 </template>
