b3f17ce4800190c950170a458644d1e759b7d379
ชื่Salisa17za <spoysalizz17@gmail.com>
2025-08-28 21:34:37 -0700

    update v2

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,17 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/show',
+    name: 'show',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/SHowproduct.vue')
+  },
+
+  
 ]
 
 const router = createRouter({
